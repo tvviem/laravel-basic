945de3cfc51d8f8737e25d012b65ada97100637a
viem trieu <trieuvinhviem@gmail.com>
2018-08-15 10:16:14 +0700

    Laravel Basic - Update and Delete Post - PostsController and show.blade.php

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -43,6 +43,7 @@ class PostsController extends Controller
             'title' => 'required',
             'body' => 'required'
         ]);
+        echo $request->input('body');
         $post = new Post;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
@@ -70,7 +71,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -82,7 +84,15 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     /**
@@ -93,6 +103,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post Removed');
     }
 }
